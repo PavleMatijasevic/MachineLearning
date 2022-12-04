c1197d6def96fee8ed7c1970ce95f1d01acc2d10
PavleMatijasevic <matijasevic.pavle99@gmail.com>
2022-12-04 19:45:10 +0100

    add 1.ipynb

--- House_Price_Prediction/1.py
+++ House_Price_Prediction/1.py
@@ -1,6 +1,11 @@
-import pandas as pd 
-import numpy as np 
-import matplotlib.pyplot as plt 
-import seaborn as sns 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
 
-
+customers = pd.read_csv('USA_Housing.csv')
+customers.head()
