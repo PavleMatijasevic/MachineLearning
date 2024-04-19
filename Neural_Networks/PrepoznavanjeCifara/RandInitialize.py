7de8e9ef926f0b49aacf310bcae02122ae73f8b9
PavleMatijasevic <matijasevic.pavle99@gmail.com>
2024-04-19 11:17:34 +0200

    Add Prepoznavanje Cifara

---
+++ Neural_Networks/PrepoznavanjeCifara/RandInitialize.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def initialise(a, b):
+	epsilon = 0.15
+	# Randomly initialises values of thetas between [-epsilon, +epsilon]
+
+	c = np.random.rand(a, b + 1) * (2 * epsilon) - epsilon
+	return c
