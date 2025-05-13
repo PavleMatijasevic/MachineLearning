72bdd5051234d71e0a594aa7b8aad2aee91ed56a
Pavle Matijasevic <matijasevic.pavle99@gmail.com>
2025-05-13 13:20:43 +0200

    update preview_track.py

--- Reinforcement Learning Projects/CarDrive/preview_track.py
+++ Reinforcement Learning Projects/CarDrive/preview_track.py
@@ -1,13 +1,13 @@
 from car_env import CarEnv
 import time
 
-# Pokreni okruženje sa prikazom
+# Pokreni okruzenje sa prikazom
 env = CarEnv(render_mode="human")
 
-# Resetuj da generiše novu stazu i centar
+# Resetuj da generise novu stazu i centar
 obs, _ = env.reset()
 env.render()
-# Pokaži stazu na ekranu 5 sekundi
+# Pokazi stazu na ekranu 10 sekundi
 time.sleep(10)
 
 env.close()
